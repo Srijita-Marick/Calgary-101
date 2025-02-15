9060371b044382f8b6d1ef78ab7faaa2a57545e9
30230880 <srijita.marick@ucalgary.ca>
2025-02-15 12:46:17 -0700

    Test commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module calgaryhacks2025.calgary101 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens calgaryhacks2025.calgary101 to javafx.fxml;
+    exports calgaryhacks2025.calgary101;
+}
