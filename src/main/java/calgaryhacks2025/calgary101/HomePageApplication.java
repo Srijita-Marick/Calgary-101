ffdb8f30013fbf28350b11cccd2c719e6e833721
30230880 <srijita.marick@ucalgary.ca>
2025-02-15 19:21:28 -0700

    homePage.fxml

--- src/main/java/calgaryhacks2025/calgary101/HomePageApplication.java
+++ src/main/java/calgaryhacks2025/calgary101/HomePageApplication.java
@@ -1,7 +1,11 @@
 package calgaryhacks2025.calgary101;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomePageApplication extends Application {
 
@@ -10,7 +14,11 @@ public class HomePageApplication extends Application {
     }
     //why
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("homePage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setTitle("Calgary 101");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
