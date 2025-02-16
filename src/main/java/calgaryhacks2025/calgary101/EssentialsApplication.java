3b0d9d01a3da0ded47b8214fb24c0ea6b0bd8ca6
30230880 <srijita.marick@ucalgary.ca>
2025-02-15 21:43:58 -0700

    Set up essentials.fxml and button actions

--- src/main/java/calgaryhacks2025/calgary101/EssentialsApplication.java
+++ src/main/java/calgaryhacks2025/calgary101/EssentialsApplication.java
@@ -1,7 +1,11 @@
 package calgaryhacks2025.calgary101;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EssentialsApplication extends Application {
 
@@ -10,6 +14,11 @@ public class EssentialsApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("essentials.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        primaryStage.setTitle("Calgary 101");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
--- src/main/java/calgaryhacks2025/calgary101/HomePageController.java
+++ src/main/java/calgaryhacks2025/calgary101/HomePageController.java
@@ -1,4 +1,33 @@
 package calgaryhacks2025.calgary101;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePageController {
+    @FXML
+    private void handleEssentialsButton(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("essentials.fxml"));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
 }
